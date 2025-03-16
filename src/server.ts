63bddceb1acd33dd8df235c7f9e4e3ed67d84430
Tony He <ghe@lakeheadu.ca>
2025-03-16 02:49:42 -0400

    feat:add new routes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,28 @@ import { MaxeyServerResponse } from './lib/types';
 
 const PORT = Number(process.env.PORT) || 8000;
 
+const USERS = [
+	{ id: 1, name: 'tony', username: 'superman566', password: 'password' },
+	{ id: 2, name: 'christy', username: 'yaling', password: 'password' },
+];
+
+type Post = {
+	id: number;
+	title: string;
+	body: string;
+	userId: number;
+	auther?: string;
+};
+
+const POSTS: Post[] = [
+	{
+		id: 1,
+		title: 'this is a post title',
+		body: 'this is a post body',
+		userId: 1,
+	},
+];
+
 const server = new MaxeyTS();
 
 server.route(
@@ -27,6 +49,39 @@ server.route(
 	'/scripts.js',
 	(req: http.IncomingMessage, res: MaxeyServerResponse) => {
 		res.sendFile('./public/scripts.js', 'text/javascript');
+	}
+);
+
+server.route(
+	'get',
+	'/favicon.ico',
+	(req: http.IncomingMessage, res: MaxeyServerResponse) => {
+		res.sendFile('./public/favicon.ico', 'image/x-icon');
+	}
+);
+
+server.route(
+	'get',
+	'/api/posts',
+	(req: http.IncomingMessage, res: MaxeyServerResponse) => {
+		const usersMap = new Map<number, string>();
+		USERS.forEach((user) => {
+			usersMap.set(user.id, user.name);
+		});
+
+		const posts = POSTS.map((post) => {
+			post.auther = usersMap.get(post.userId);
+			return post;
+		});
+		res.status(200).json(posts);
+	}
+);
+
+server.route(
+	'get',
+	'/api/user',
+	(req: http.IncomingMessage, res: MaxeyServerResponse) => {
+		res.status(200).json(USERS);
 	}
 );
 
